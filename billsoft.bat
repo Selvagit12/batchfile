b3e1302a065f0b654e62810d7d36744fec60c822
Selvagit12 <116102267+Selvagit12@users.noreply.github.com>
2024-01-10 18:49:16 +0530

    Add files via upload

---
+++ billsoft.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+start cmd.exe /C "C:\Users\Bharathi\Documents\projects\jewellery_billing\env\Scripts\python.exe C:\Users\Bharathi\Documents\projects\jewellery_billing\billing_system\manage.py runserver && cd D:\D1\ && D:"
+start C:\"Program Files"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/"
